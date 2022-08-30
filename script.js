347e1c560a2974ca2ec894118496d3316508c381
Your Name <jenaroluciano@gmail.com>
2022-08-30 11:54:13 -0300

    Terminada

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
-
-
-const display = document.querySelector(".display");
-let expresion = "";
-
 const Clear = document.querySelector(".btn-clear");
 const Delete = document.querySelector(".btn-delete");
 const Equal = document.querySelector(".btn-equal");
-
-
-
+const display = document.querySelector(".display");
 const btsOps = [...document.querySelectorAll(".btn-operational")];
+let expresion = "";
 
 
 btsOps.forEach(function (btn) {
@@ -19,7 +13,6 @@ btsOps.forEach(function (btn) {
 
   });
 });
-
 
 Delete.addEventListener("click", (e) => {
   const str = display.textContent.slice(0, -1);
@@ -33,11 +26,32 @@ Clear.addEventListener("click", (e) => {
 });
 
 Equal.addEventListener("click", (e) => {
-  try {
-    display.textContent = eval(expresion);
-  } catch {
-    display.textContent = "Syntax error";
-  } finally {
-    expresion = "";
-  }
+  let potencia = [...expresion.split("^")];
+  let raiz = [...expresion.split("√")];
+
+  if (potencia.length == 2) {
+    display.textContent = pow(potencia[0], potencia[1]);
+  } else if (raiz.length == 2) {
+    display.textContent = sqrt(raiz[1], raiz[0]);
+  } else
+    try {
+      display.textContent = eval(expresion);
+      expresion = eval(expresion);
+    } catch {
+      expresion = " ";
+      display.textContent = "Syntax Error";
+    }
 });
+
+
+const pow = (n, p) => {
+  let res = 1;
+  for (let i = 0; i < p; i++) {
+    res = res * n;
+  }
+  return res;
+};
+
+const sqrt = (n, s) => {
+  return Math.pow(n, 1 / s);
+};
